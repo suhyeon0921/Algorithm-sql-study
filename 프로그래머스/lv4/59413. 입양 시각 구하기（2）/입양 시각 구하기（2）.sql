c3857fb2953b51f0a780104803b076799ae5af7e
Suhyeon Lee <105929978+suhyeon3484@users.noreply.github.com>
2023-03-04 02:16:46 +0900

    Update 입양 시각 구하기（2）.sql

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -7,12 +7,3 @@ select (@hour := @hour + 1) as HOUR
 from ANIMAL_OUTS ao
 where @hour < 23
 order by HOUR asc
-
-
-# set @hour := -1;
- 
-# select ( @hour := @hour + 1 ) as hour
-#     , (select count(ao2.animal_id) from animal_outs ao2 where date_format(ao2.datetime, '%H') = @hour ) as cnt 
-# from animal_outs ao
-# where @hour < 23 
-# order by ( @hour := @hour + 1 )  asc
