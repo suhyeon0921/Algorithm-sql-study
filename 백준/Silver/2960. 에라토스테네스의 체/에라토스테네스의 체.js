cac41255fcf6f3e3c592bae4d475c3de5270abc9
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-03-15 01:19:40 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 124 ms, Memory: 9408 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.js
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.js
@@ -1,18 +1,31 @@
-const [n, k] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(v => +v);
+const [n, k] = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((v) => +v);
+
 const arr = [];
-for (let i=2; i<=n; i++) arr.push(i);
-let count = 0;
-let prime;
-let nth;
-while (count < k) {
-    prime = arr[0];
-    arr.some(v => {
-        if (v % prime === 0) {
-            arr.splice(arr.indexOf(v), 1);
-            nth = v
-            count++;
-        }
-        if (count === k) return true;
-    });
+for (let i = 2; i <= n; i++) {
+  arr.push(i);
 }
-console.log(nth);
+let cnt = 0;
+let prime;
+let answer;
+
+while (cnt < k) {
+  prime = arr[0];
+  arr.some((v) => {
+    if (v % prime == 0) {
+      // 배수 제거
+      arr.splice(arr.indexOf(v), 1);
+      answer = v;
+      cnt++;
+    }
+    if (cnt === k) {
+      return true;
+    }
+  });
+}
+
+console.log(answer);
