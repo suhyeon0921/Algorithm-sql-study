7fbef37bbe0ebc79fdfb92269f2d422342a4b2f2
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-04-27 02:01:05 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    const nums = s.split(' ');
+    var answer = [];
+    answer.push(Math.min(...nums));
+    answer.push(Math.max(...nums));
+    return answer.join(' ');
+}
