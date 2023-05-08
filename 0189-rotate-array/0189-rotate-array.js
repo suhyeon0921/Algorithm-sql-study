b23d54dcb0f16a8aeaa3aaca0c4e6f92b77d9b95
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-05-09 01:37:50 +0900

    Time: 114 ms (14.66%), Space: 60.1 MB (5.50%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,12 @@
+var rotate = function(nums, k) {
+
+for (let i = nums.length - 1; i >= 0; i--) {
+   nums[i + k] = nums[i]
+}
+
+for (let i =  k - 1; i >=0; i--) {
+  nums[i] = nums.pop()
+}
+    
+    return nums
+};
