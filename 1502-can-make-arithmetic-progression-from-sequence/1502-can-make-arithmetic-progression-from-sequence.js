4fcee4d45dd879d2b0bd9ff7814de61d4d3d9bb6
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-06-08 11:33:35 +0900

    Time: 58 ms (74.25%), Space: 42.4 MB (41.60%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,12 @@
+var canMakeArithmeticProgression = function(arr) {
+  arr.sort((a, b) => a - b);
+  
+  commonDifference = arr[1] - arr[0];
+  
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== commonDifference) {
+      return false;
+    }
+  }
+  return true;
+};
