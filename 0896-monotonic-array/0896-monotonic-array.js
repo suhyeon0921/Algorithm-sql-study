ef120af69a965b5fafa012c608e9ae7bbf02db5c
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-06-10 00:22:26 +0900

    Time: 102 ms (30.72%), Space: 53.5 MB (33.33%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+  let increasing = true;
+  let decreasing = true;
+  
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > nums[i]) {
+      increasing = false;
+    }
+    if (nums[i - 1] < nums[i]) {
+      decreasing = false;
+    }
+  }
+  return increasing || decreasing;
+};
