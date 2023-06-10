cef3d7f815d68bcebc0d22e32ce005015b19d643
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-06-11 02:15:40 +0900

    Time: 179 ms (59.21%), Space: 77.4 MB (31.25%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
@@ -0,0 +1,17 @@
+var findSmallestSetOfVertices = function(n, edges) {
+  const indegree = new Array(n).fill(0);
+
+  for (let edge of edges) {
+    indegree[edge[1]]++;
+  }  
+  
+  const res = [];
+
+  for (let i in indegree) {
+    if (indegree[i] === 0) {
+      res.push(i);
+    }
+  }
+
+  return res;
+};
