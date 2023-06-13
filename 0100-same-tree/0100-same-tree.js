b74d138231424bf1c6bf32f96e8e57eca6db5bf1
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2023-06-14 00:14:27 +0900

    Time: 49 ms (94.20%), Space: 41.5 MB (98.92%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,12 @@
+var isSameTree = function(p, q) {
+  if (!p && !q) {
+    return true;
+  }
+  if (p && !q || q && !p) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
