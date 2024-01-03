6c87c2ad2748cd2f81ef5c8c5fd0474fa9656ea3
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2024-01-02 21:12:25 -0800

    Time: 33 ms (94.70%), Space: 17.1 MB (20.81%) - LeetHub

---
+++ LeetCode/0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count_s = Counter(s)
+        count_t = Counter(t)
+        
+        for char in count_t:
+            if count_t[char] != count_s[char]:
+                return char
