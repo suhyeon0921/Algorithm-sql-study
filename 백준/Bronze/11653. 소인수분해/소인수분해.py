e0d949a506ddf0f6c924eb9be830261a519ace7b
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2024-02-10 05:52:26 -0800

    [Bronze I] Title: 소인수분해, Time: 900 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+def factorization(x):
+    d = 2
+
+    while d <= x:
+        if x % d == 0:
+            print(d)
+            x = x / d
+        else:
+            d = d + 1
+
+factorization(N)
