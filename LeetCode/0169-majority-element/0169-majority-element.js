f2cd24b39b405fd5fbbcb5e9c0df1919dd4d6b9b
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2024-04-22 21:19:40 +0900

    Time: 47 ms (97.30%), Space: 50.8 MB (87.54%) - LeetHub

---
+++ LeetCode/0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  let count = 0;
+  let candidate
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+      count = 1;
+    } else if (candidate === nums[i]) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return candidate;
+};
