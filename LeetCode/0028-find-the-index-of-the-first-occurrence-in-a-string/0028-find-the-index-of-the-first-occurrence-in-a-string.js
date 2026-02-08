aa2880d81b55336674134c6cdf6a049c9060015e
Suhyeon Lee <105929978+suhyeon0921@users.noreply.github.com>
2026-02-08 20:35:04 +0900

    Time: 1 ms (46.82%), Space: 53.1 MB (94.12%) - LeetHub

--- LeetCode/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ LeetCode/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,12 +1,6 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
 var strStr = function(haystack, needle) {
-  if (haystack.includes(needle)) {
-    return haystack.indexOf(needle);
-  } else {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) return i;
+    }
     return -1;
-  }
 };
